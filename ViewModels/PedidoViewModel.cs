312f06722d3310bfec654ed52bad64292731b322
Sebastião Leal <sebastiaodeoleal@gmail.com>
2025-09-12 16:11:14 -0300

    feat(viewmodels): adicionar ProdutoViewModel e PedidoViewModel com CRUD básico

---
+++ ViewModels/PedidoViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using WpfAppCadastro.Models;
+using WpfAppCadastro.Services;
+
+namespace WpfAppCadastro.ViewModels
+{
+    public class PedidoViewModel : BaseViewModel
+    {
+        private PedidoService _pedidoService;
+
+        public ObservableCollection<Pedido> Pedidos { get; set; }
+
+        private Pedido _pedidoSelecionado;
+        public Pedido PedidoSelecionado
+        {
+            get => _pedidoSelecionado;
+            set
+            {
+                _pedidoSelecionado = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AdicionarCommand { get; }
+        public ICommand AtualizarCommand { get; }
+        public ICommand RemoverCommand { get; }
+
+        public PedidoViewModel()
+        {
+            _pedidoService = new PedidoService();
+            Pedidos = new ObservableCollection<Pedido>(_pedidoService.ObterTodos());
+
+            AdicionarCommand = new RelayCommand(AdicionarPedido);
+            AtualizarCommand = new RelayCommand(AtualizarPedido);
+            RemoverCommand = new RelayCommand(RemoverPedido);
+        }
+
+        private void AdicionarPedido(object parameter)
+        {
+            if (PedidoSelecionado != null)
+            {
+                _pedidoService.Adicionar(PedidoSelecionado);
+                Pedidos.Add(PedidoSelecionado);
+                PedidoSelecionado = new Pedido(); /* limpa form */
+            }
+        }
+
+        private void AtualizarPedido(object parameter)
+        {
+            if (PedidoSelecionado != null)
+            {
+                _pedidoService.Atualizar(PedidoSelecionado);
+                Pedidos = new ObservableCollection<Pedido>(_pedidoService.ObterTodos());
+                OnPropertyChanged(nameof(Pedidos));
+            }
+        }
+
+        private void RemoverPedido(object parameter)
+        {
+            if (PedidoSelecionado != null)
+            {
+                _pedidoService.Remover(PedidoSelecionado.Id);
+                Pedidos.Remove(PedidoSelecionado);
+                PedidoSelecionado = null;
+            }
+        }
+    }
+}
---
+++ ViewModels/ProdutoViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Windows.Input;
+using WpfAppCadastro.Models;
+using WpfAppCadastro.Services;
+
+namespace WpfAppCadastro.ViewModels
+{
+    public class ProdutoViewModel : BaseViewModel
+    {
+        private ProdutoService _produtoService;
+
+        public ObservableCollection<Produto> Produtos { get; set; }
+
+        private Produto _produtoSelecionado;
+        public Produto ProdutoSelecionado
+        {
+            get => _produtoSelecionado;
+            set
+            {
+                _produtoSelecionado = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AdicionarCommand { get; }
+        public ICommand AtualizarCommand { get; }
+        public ICommand RemoverCommand { get; }
+
+        public ProdutoViewModel()
+        {
+            _produtoService = new ProdutoService();
+            Produtos = new ObservableCollection<Produto>(_produtoService.ObterTodos());
+
+            AdicionarCommand = new RelayCommand(AdicionarProduto);
+            AtualizarCommand = new RelayCommand(AtualizarProduto);
+            RemoverCommand = new RelayCommand(RemoverProduto);
+        }
+
+        private void AdicionarProduto(object parameter)
+        {
+            if (ProdutoSelecionado != null)
+            {
+                _produtoService.Adicionar(ProdutoSelecionado);
+                Produtos.Add(ProdutoSelecionado);
+                ProdutoSelecionado = new Produto(); /* limpa form */
+            }
+        }
+
+        private void AtualizarProduto(object parameter)
+        {
+            if (ProdutoSelecionado != null)
+            {
+                _produtoService.Atualizar(ProdutoSelecionado);
+                Produtos = new ObservableCollection<Produto>(_produtoService.ObterTodos());
+                OnPropertyChanged(nameof(Produtos));
+            }
+        }
+
+        private void RemoverProduto(object parameter)
+        {
+            if (ProdutoSelecionado != null)
+            {
+                _produtoService.Remover(ProdutoSelecionado.Id);
+                Produtos.Remove(ProdutoSelecionado);
+                ProdutoSelecionado = null;
+            }
+        }
+    }
+}
