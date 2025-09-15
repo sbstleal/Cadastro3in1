9acdd2ca5b012aaa77e839513ddec9986615adfc
Sebastião Leal <sebastiaodeoleal@gmail.com>
2025-09-15 10:15:05 -0300

    feat(views): adicionar tela de Produto com CRUD e binding ao ProdutoViewModel

---
+++ Views/ProdutoView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace WpfAppCadastro.Views
+{
+    public partial class ProdutoView : UserControl
+    {
+        public ProdutoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
