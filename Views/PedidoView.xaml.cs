3ee819d954735c026bbe0d4ea421c00b5c4abdaa
Sebastião Leal <sebastiaodeoleal@gmail.com>
2025-09-15 10:17:06 -0300

    feat(views): adicionar tela de Pedido com CRUD e binding ao PedidoViewModel

---
+++ Views/PedidoView.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows.Controls;
+
+namespace WpfAppCadastro.Views
+{
+    public partial class PedidoView : UserControl
+    {
+        public PedidoView()
+        {
+            InitializeComponent();
+        }
+    }
+}
